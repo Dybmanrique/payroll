6b6d8ceb074a5d3394a2c3387cbf5c38b2a28f61
Deyber <dbrmanrique@gmail.com>
2024-10-14 17:01:54 -0500

    ajustando padding

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-row items-center px-10 rounded-md bg-white shadow-sm">
+<div class="flex flex-row items-center px-10 py-2 rounded-md bg-white shadow-sm">
     <img src="{{ asset('img/logo.png') }}" alt="Logo" {{ $attributes }}>
     <h2 class="text-4xl">PLANILLAS <span class="font-bold">UGEL</span></h2>
 </div>
