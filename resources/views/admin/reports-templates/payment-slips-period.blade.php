ff80f30fadc531f542f7e99cd8de6918dac485db
Deyber <dbrmanrique@gmail.com>
2024-11-02 18:26:53 -0500

    Cambiando el orden

--- resources/views/admin/reports-templates/payment-slips-period.blade.php
+++ resources/views/admin/reports-templates/payment-slips-period.blade.php
@@ -184,16 +184,16 @@
                                         <td class="text-right">{{ $payment->life_insurance_afp }}</td>
                                     </tr>
                                 @endif
-                                @if ($payment->fines_discount)
-                                    <tr>
-                                        <td>Multas: </td>
-                                        <td class="text-right">{{ $payment->fines_discount }}</td>
-                                    </tr>
-                                @endif
                                 @if ($payment->cuarta)
                                     <tr>
                                         <td>Desc. 4ta Categoría: </td>
                                         <td class="text-right">{{ $payment->cuarta }}</td>
+                                    </tr>
+                                @endif
+                                @if ($payment->fines_discount)
+                                    <tr>
+                                        <td>Multas: </td>
+                                        <td class="text-right">{{ $payment->fines_discount }}</td>
                                     </tr>
                                 @endif
                             </table>
