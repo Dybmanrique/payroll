f2c891d320014d4217c2c294e18c8f1fca48b748
Deyber <dbrmanrique@gmail.com>
2024-11-02 15:10:01 -0500

    Cambiando la ubicación

--- public/js/admin/message_toastr.js
+++ public/js/admin/message_toastr.js
@@ -3,7 +3,7 @@ toastr.options = {
     "debug": false,
     "newestOnTop": false,
     "progressBar": false,
-    "positionClass": "toast-top-center",
+    "positionClass": "toast-bottom-right",
     "preventDuplicates": false,
     "onclick": null,
     "showDuration": "300",
