1bc8d1968c183906670bc5c80a5a7e0f444760fb
Deyber <dbrmanrique@gmail.com>
2024-11-24 11:58:41 -0500

    Agreagando el campo de total de aportes

--- database/migrations/2024_10_24_111600_create_payments_table.php
+++ database/migrations/2024_10_24_111600_create_payments_table.php
@@ -13,18 +13,19 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->decimal('basic',9,2);
-            $table->decimal('afp_discount',9,2)->nullable();
-            $table->decimal('onp_discount',9,2)->nullable();
-            $table->decimal('fines_discount',9,2)->nullable();
-            $table->decimal('total_remuneration',9,2)->default(0);
-            $table->decimal('total_discount',9,2)->default(0);
-            $table->decimal('net_pay',9,2)->default(0);
-            $table->decimal('essalud',9,2)->nullable();
-            $table->decimal('cuarta',9,2)->nullable();
-            $table->decimal('obligatory_afp',9,2)->nullable();
-            $table->decimal('life_insurance_afp',9,2)->nullable();
-            $table->decimal('variable_afp',9,2)->nullable();
+            $table->decimal('basic', 9, 2);
+            $table->decimal('afp_discount', 9, 2)->nullable();
+            $table->decimal('onp_discount', 9, 2)->nullable();
+            $table->decimal('fines_discount', 9, 2)->nullable();
+            $table->decimal('total_remuneration', 9, 2)->default(0);
+            $table->decimal('total_discount', 9, 2)->default(0);
+            $table->decimal('total_contribution', 9, 2)->default(0);
+            $table->decimal('net_pay', 9, 2)->default(0);
+            $table->decimal('essalud', 9, 2)->nullable();
+            $table->decimal('cuarta', 9, 2)->nullable();
+            $table->decimal('obligatory_afp', 9, 2)->nullable();
+            $table->decimal('life_insurance_afp', 9, 2)->nullable();
+            $table->decimal('variable_afp', 9, 2)->nullable();
             $table->integer('days')->default(30);
             $table->integer('days_discount')->nullable();
             $table->integer('hours_discount')->nullable();
